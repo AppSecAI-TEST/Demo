2189f965683d615dd9239ef4d7f1de7225df85eb
Deepa Kalyani Ramasubbu <dramasubbu@altimetrik.com>
2017-08-08 13:47:00 +0530

    'code'

---
+++ src/main/java/learn/fullstack/ngspring/demo/DemoAppServerApplication.java
@@ -0,0 +1,12 @@
+package learn.fullstack.ngspring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppServerApplication.class, args);
+	}
+}
